f5c83faeea620c2b680f03a0ba33ec1b1e1e9846
Kacper Perzankowski <perzankowski.kacper@gmail.com>
2019-03-17 16:17:46 +0100

    migration script file

--- scripts/db-migration.sh
+++ scripts/db-migration.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-diesel migration $1 \
-  --database-url postgres://developer:developer@localhost:5432/cqrs_example_$2 \
-  --migration-dir migrations/$2 \
-  --config-file diesel-$2.toml
-echo "migration $1, db: $2 - Ok!"
+. .env
+
+cmd=$1
+dbtype=$2
+
+if [ $dbtype = "read" ]
+then
+  connection=$DATABASE_URL_READ
+else
+  connection=$DATABASE_URL_WRITE
+fi
+
+diesel migration $cmd \
+  --database-url $connection \
+  --migration-dir migrations/$dbtype \
+  --config-file diesel-$dbtype.toml
+
+echo "migration $cmd, db: $dbtype - Ok!"
